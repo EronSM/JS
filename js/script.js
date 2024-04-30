6488116cd88ba42563659183b396bcc37081e354
EronSM <164966687+EronSM@users.noreply.github.com>
2024-04-30 17:07:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const texto = document.getElementById('texto');
 const rquad = document.getElementById('rquad');
 const int = document.getElementById('int');
 
-
 numeroTitulo.innerHTML = numero;
 texto.innerHTML = `<p> Seu Número +2 é ${numero + 2} </p>`;
 rquad.innerHTML = `Raiz quadrada: ${numero ** 0.5}`;
