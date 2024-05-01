1f46229d681cf7981e654478203f59eac97b6ee3
EronSM <164966687+EronSM@users.noreply.github.com>
2024-05-01 01:36:38 +0000

    Alteração

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const numero = Number(prompt('Digite um numero'));
 const numeroTitulo = document.getElementById('numero-titulo');
-const texto = document.getElementById('texto');
-const rquad = document.getElementById('rquad');
-const int = document.getElementById('int');
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p> Seu Número +2 é ${numero + 2} </p>`;
-rquad.innerHTML = `Raiz quadrada: ${numero ** 0.5}`;
-int.innerHTML = `${numero} é inteiro: ${Number.isInteger(numero)}`;
+
+const texto = document.getElementById('texto');
+
+texto.innerHTML = '';
+texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5}</p>`;
+texto.innerHTML += `<p> ${numero} é inteiro: ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p> É NaN: ${Number.isNaN(numero)} </p>`;
+texto.innerHTML += `<p> Arredondamento para baixo: ${Math.floor(numero)}`;
+texto.innerHTML += `<p> Arredondamento para cima: ${Math.ceil(numero)} </p>`;
+texto.innerHTML += `<p> Com duas casas decimais: ${numero.toFixed(2)}`;
